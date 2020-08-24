c3a99177fe837341de6508edd78747cf575838c2
ericloureiro <ericloureiro@gmail.com>
2020-08-24 22:56:37 +0100

    feat: GamePage now has a input and a randomize encrypted awnser

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import '../App.css';
-import { withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Container, Row } from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import '../index.css';
+import InputComponent from '../components/input-component';
+import data from '../resources/words.json';
 
-class GamePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = props.history.location.state;
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(6),
+      height: theme.spacing(6),
+    },
+  },
+}));
 
-  render() {
-    return (
-      <div className='centered'>
-        {console.log(this.state)}
-        <div>Chegou</div>
-      </div>
-    );
-  }
+export default function GamePage() {
+  const classes = useStyles();
+  const [value, setValue] = useState('');
+
+  const words = data.map((word) => word);
+
+  const awnser = words[Math.floor(Math.random() * words.length)];
+
+  const renderEncryptedAwnser = (awnser) => {
+    const items = [];
+
+    for (let i = 0; i < awnser.length; i++) {
+      items.push(<Paper key={i} variant='outlined' color='primary' />);
+    }
+
+    return <div className={classes.root}>{items}</div>;
+  };
+
+  return (
+    <div className='centered'>
+      <Container>
+        <Row>{renderEncryptedAwnser(awnser)}</Row>
+        <Row className='centerfixed'>
+          <InputComponent
+            placeholder={'TRY TO GUESS A LETTER'}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </Row>
+      </Container>
+    </div>
+  );
 }
-
-export default withRouter(GamePage);
