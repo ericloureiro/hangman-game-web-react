959bfdbd0df073e27747132d273b7707335d29da
ericloureiro <ericloureiro@gmail.com>
2020-08-24 22:54:52 +0100

    refactor: changed from React.Component to function. Testing to see which one behaves better

--- src/components/select-component.js
+++ src/components/select-component.js
@@ -1,47 +1,32 @@
 import React from 'react';
 import { Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
 
-class SelectComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: props.value };
+export default function SelectComponent(props) {
+  const { title, value, valuesList, handleChange, width } = props;
 
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-  };
-
-  render() {
-    let { title, value, valuesList, handleChange, width } = this.props;
-
-    return (
-      <FormControl
-        style={{
-          width,
-          position: 'flex',
-          bottom: 10,
-          right: 15,
-          margin: 5,
-        }}
+  return (
+    <FormControl
+      style={{
+        width,
+        position: 'flex',
+        bottom: 10,
+        right: 15,
+        margin: 5,
+      }}
+    >
+      <InputLabel>{title}</InputLabel>
+      <Select
+        variant={'standard'}
+        value={value}
+        onChange={handleChange}
+        label={title}
       >
-        <InputLabel>{title}</InputLabel>
-        <Select
-          variant={'standard'}
-          value={value}
-          onChange={handleChange}
-          label={title}
-        >
-          {valuesList.map((value, i) => (
-            <MenuItem key={i} value={value}>
-              {value}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    );
-  }
+        {valuesList.map((value, i) => (
+          <MenuItem key={i} value={value}>
+            {value}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 }
-
-export default SelectComponent;
