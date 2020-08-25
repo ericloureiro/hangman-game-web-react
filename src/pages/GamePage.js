e760d51c0dc6690a0bb5eba001e3116026da549a
ericloureiro <eric.loureiro@gmail.com>
2020-08-25 23:14:32 +0100

    feat: add visual correspondence to letter on encrypted word
    add SnackBarComponent and its controller to page

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -4,6 +4,7 @@ import { Container, Row } from 'reactstrap';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputComponent from '../components/input-component';
+import SnackBarComponent from '../components/snackbar-component';
 import data from '../resources/words.json';
 
 const useStyles = makeStyles((theme) => ({
@@ -21,14 +22,51 @@ const GamePage = (props) => {
   const classes = useStyles();
   const [value, setValue] = useState('');
   const [word, setWord] = useState('');
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [severity, setSeverity] = useState('success');
+  const [textSnackBar, setTextSnackBar] = useState('');
   const words = data.map((word) => word);
   const awnser = words[Math.floor(Math.random() * words.length)];
+
+  const space = '_';
+
+  const onEnterPressed = (input) => {
+    console.log(value);
+    if (value === '' || value === undefined) {
+      setTextSnackBar("You can't submit an empty input");
+      setSeverity('warning');
+      setOpenSnackBar(true);
+    }
+
+    // TODO Validations before replace !!
+    // if () {
+    //   setTextSnackBar('You guessed the letter ' + value);
+    //   setOpenSnackBar(true);
+    // }
+    // && input.Length == 1
+    // && char.IsLetter(input, 0)
+    // && !awnserEncrypted.ToString().Contains(input)
+    // && !displayedTrys.ToString().Contains(input);
+    // awnser.replace(value, awnser);
+
+    setValue('');
+  };
 
   useEffect(() => {
     const items = [];
 
     for (let i = 0; i < awnser.length; i++) {
-      items.push(<Paper key={i} variant='outlined' color='primary' />);
+      items.push(
+        <Paper
+          className='centered'
+          key={i}
+          variant='elevation'
+          elevation={2}
+          color='primary'
+        >
+          <h1>{space}</h1>
+        </Paper>
+      );
     }
 
     setWord(<div className={classes.root}>{items}</div>);
@@ -37,16 +75,23 @@ const GamePage = (props) => {
 
   return (
     <div className='centered'>
-      <Container>
+      <Container className='centerfixed'>
         <Row>{word}</Row>
         <Row className='centerfixed'>
           <InputComponent
             placeholder={'TRY TO GUESS A LETTER'}
             value={value}
-            onChange={(e) => setValue(e.target.value)}
+            handleChange={(e) => setValue(e.target.value)}
+            handleEnterPressed={(e) => onEnterPressed()}
           />
         </Row>
       </Container>
+      <SnackBarComponent
+        severity={severity}
+        text={textSnackBar}
+        openSnackBar={openSnackBar}
+        setOpenSnackBar={setOpenSnackBar}
+      />
     </div>
   );
 };
