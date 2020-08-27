6ed40e15f179e5d401b0c8750ae2da0107d29feb
ericloureiro <eric.loureiro@gmail.com>
2020-08-27 22:40:09 +0100

    refactor: from Component to FC

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -3,23 +3,21 @@ import GamePage from '../pages/GamePage';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-class RouterController extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path='/about' component={About}></Route>
-          <Route path='/game'>
-            <GamePage />
-          </Route>
-          <Route path='/'>
-            <HomePage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const RouterController = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/about' component={About}></Route>
+        <Route path='/game'>
+          <GamePage />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 function About() {
   return <h2>About</h2>;
