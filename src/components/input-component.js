f239a0eba9b2abb202b1b612b5056ea5e6580061
ericloureiro <eric.loureiro@gmail.com>
2020-08-27 22:43:46 +0100

    feat: add regex to chek user input and disable send on error

--- src/components/input-component.js
+++ src/components/input-component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
@@ -28,27 +28,41 @@ const useStyles = makeStyles((theme) => ({
 
 const InputComponent = (props) => {
   const classes = useStyles();
-  const { placeholder, value, handleSubmit, handleChange } = props;
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
+  const { placeholder, handleSubmit } = props;
+
+  const regex = useMemo(() => new RegExp('[^A-Za-z]'), []);
+  const handleChange = useCallback((value) => {
+    setValue(value);
+  }, []);
+
+  useEffect(() => setError(!regex.test(value)), [regex, value]);
 
   return (
     <Paper component='form' className={classes.root}>
       <InputBase
         autoFocus
+        error={error}
         className={classes.input}
         placeholder={placeholder}
         value={value.toUpperCase()}
-        onChange={handleChange}
+        onChange={(e) => handleChange(e.target.value)}
         onKeyPress={(e) => {
-          if (e.key === 'Enter') {
+          if (e.key === 'Enter' && !error) {
             e.preventDefault();
             handleSubmit();
           }
         }}
         type={'text'}
-        inputProps={{ maxLength: 1, pattern: '[a-zA-Z]' }}
+        inputProps={{
+          maxLength: 1,
+          pattern: '[a-zA-Z]',
+        }}
       />
       <Divider className={classes.divider} orientation='vertical' />
       <IconButton
+        disabled={!error}
         className={classes.iconButton}
         color={'primary'}
         onClick={(e) => handleSubmit()}
